4137fa55d79c620d8d3bec16b0bcac7eedcdde67
Philippe Matray <phmatray@gmail.com>
2019-11-10 19:55:23 +0100

    Fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { EventFourthPageComponent } from './pages/event-fourth/event-fourth-page.component';
 import { OrganizingTeamPageComponent } from './pages/organizing-team/organizing-team-page.component';
 import { PartnersPageComponent } from './pages/partners/partners-page.component';
 import { NotFoundPageComponent } from './pages/not-found/not-found-page.component';
 
 const routes: Routes = [
-  { path: '', component: EventFourthPageComponent },
+  { path: 'event', component: EventFourthPageComponent },
   { path: 'organizing-team', component: OrganizingTeamPageComponent },
   { path: 'partners', component: PartnersPageComponent},
+  { path: '',
+    redirectTo: '/event',
+    pathMatch: 'full'
+  },
   { path: '**', component: NotFoundPageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(
     routes,
-    { enableTracing: true })
+    { enableTracing: false })
   ],
   exports: [RouterModule]
 })
